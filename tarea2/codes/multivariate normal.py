9443e5a3fad442391a689981af9693b72958c712
sebastian-aranda <sebastian.arandas@alumnos.usm.cl>
2017-10-21 23:31:50 -0300

    Primeros pasos

---
+++ tarea2/codes/multivariate normal.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy
+
+mean = [0,0]
+cov = [[1,0],[0,100]]
+cov2 = [[1,0],[0,1]]
+cov3 = [[30,0],[0,100]]
+
+x,y = numpy.random.multivariate_normal(mean,cov3,5000).T
+plt.plot(x,y,'x')
+plt.title("Cov1")
+plt.axis('equal')
+plt.show()
+
