92abd1a1f52e6eb0da13c09df60b7c8482692fc3
sebastian-aranda <sebastian.arandas@alumnos.usm.cl>
2017-10-25 00:52:12 -0300

    Avances seccion 1 y 2

--- tarea2/codes/multivariate normal.py
+++ tarea2/codes/multivariate normal.py
@@ -7,7 +7,7 @@ cov2 = [[1,0],[0,1]]
 cov3 = [[30,0],[0,100]]
 
 x,y = numpy.random.multivariate_normal(mean,cov3,5000).T
-plt.plot(x,y,'x')
+plt.plot(x,y,'bo')
 plt.title("Cov1")
 plt.axis('equal')
 plt.show()
